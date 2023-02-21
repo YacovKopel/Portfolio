51c2b00d0f4a76956ee727a2ea3ad6b9a819fbf3
Yacov Kopel <kokokopel94@gmail.com>
2023-02-21 15:37:39 -0800

    data for components

---
+++ portfolio-app/src/data.js
@@ -0,0 +1,46 @@
+export const projects = [
+    {
+      title: "OneStopShop",
+      subtitle: "Full-Stack App",
+      description:
+        "Full-Stack app using Node.js and Express to create RESTful API, Handlebars for Templating engine, and MySQL and Sequelize ORM for the Database",
+      image: "./home register.jpeg",
+      link: "http://still-mesa-87070.herokuapp.com/",
+    },
+    {
+      title: "FlexFly",
+      subtitle: "Interactive Front-end App",
+      description:
+        "App that allows users to find cheap flights during selected dates and suggested events during trip duration. App used Bootstrap and Tailwind for CSS framework as well as Server-side APIs,",
+      image: "./flexfly.jpeg",
+      link: " https://yacovkopel.github.io/FlexFly/",
+    },
+    {
+      title: "Password Generator",
+      subtitle: "HTML CSS & JavaScript",
+      description:
+        "App allows users to select certain parameters to create a unique password.",
+      image: ".screenshot.jpeg.jpeg",
+      link: "https://github.com/YacovKopel/Pa55w0rdMAker",
+    },
+    {
+      title: "Weather4U",
+      subtitle: "Third-Party API and localStorage",
+      description:
+        "App uses third-party OpenWeather API to receive data for 5-Day weather forecast based on user city input.",
+      image: "./Weather4U.jpeg",
+      link: "https://github.com/YacovKopel/Weather4U",
+    },
+  ];
+
+  
+  export const skills = [
+    "JavaScript",
+    "React",
+    "Node.js",
+    "MERN",
+    "GraphQL",
+    "Python",
+    "AWS Cloud Services",
+    "MongoDB"
+  ];
