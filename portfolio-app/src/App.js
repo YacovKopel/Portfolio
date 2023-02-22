fc8cabd2c41673fd4975bdff735b00113a5dffc6
Yacov Kopel <kokokopel94@gmail.com>
2023-02-22 14:07:53 -0800

    added nav and aboout

--- portfolio-app/src/App.js
+++ portfolio-app/src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 import About from "./components/About";
-import Contact from "./components/Contact";
+// import Contact from "./components/Contact";
 import Navbar from "./components/Navbar";
-import Projects from "./components/Projects";
-import Skills from "./components/Skills";
+// import Projects from "./components/Projects";
+// import Skills from "./components/Skills";
 
 
 export default function App() {
@@ -11,9 +11,9 @@ export default function App() {
     <main className="text-gray-400 bg-gray-900 body-font">
       <Navbar />
       <About />
-      <Projects />
+      {/* <Projects />
       <Skills />
-      <Contact />
+      <Contact /> */}
     </main>
   );
 }
---
+++ portfolio-app/webpack.config.js
@@ -0,0 +1,45 @@
+
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [
+                  require('tailwindcss'),
+                  require('autoprefixer'),
+                ],
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'public/index.html',
+    }),
+  ],
+};
